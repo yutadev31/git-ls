0f8c90df80eb21d127a801b357fcd2da9d820ee3
yuta <jp.yuta0131@gmail.com>
2024-10-04 16:29:58 +0900

    refactor: remotes.rsにproc_dir関数を追加

--- src/subcommands/remotes.rs
+++ src/subcommands/remotes.rs
@@ -5,6 +5,58 @@ use git2::Repository;
 
 use crate::utils::{get_dir_items, get_git_url, home_dir_mark, print_ls_item};
 
+fn proc_dir(path: PathBuf, repository_only: bool, remote_only: bool, domain: String, user: String) {
+    let dir_path = path.clone();
+    let dir_path = dir_path.to_str().unwrap();
+    let mut dir_path = home_dir_mark(dir_path).expect("");
+
+    let repo = match Repository::open(path) {
+        Ok(data) => data,
+        Err(_) => {
+            if repository_only {
+                return;
+            }
+
+            print_ls_item(dir_path.as_str(), false, None, None);
+            return;
+        }
+    };
+
+    let remote_names = repo.remotes().unwrap();
+    let remote_names: Vec<&str> = remote_names.iter().map(|s| s.unwrap()).collect();
+    let mut remotes: HashMap<String, String> = HashMap::new();
+    for name in remote_names {
+        let url = repo.find_remote(name).unwrap().url().unwrap().to_string();
+        remotes.insert(name.to_string(), url);
+    }
+
+    for (name, url) in &remotes {
+        let url = get_git_url(url);
+        match url {
+            Some(url) => {
+                if !domain.is_empty() && !domain.eq(&url.domain) {
+                    return;
+                }
+
+                if !user.is_empty() && !user.eq(&url.user) {
+                    return;
+                }
+
+                print_ls_item(dir_path.as_str(), true, Some(name.to_string()), Some(url));
+            }
+            None => {
+                if remote_only {
+                    return;
+                }
+
+                print_ls_item(dir_path.as_str(), false, None, None);
+            }
+        }
+
+        dir_path = String::new();
+    }
+}
+
 pub fn ls_remotes(path: String, repository_only: bool, domain: String, user: String) -> Result<()> {
     let remote_only = !domain.is_empty() || !user.is_empty();
     let repository_only = repository_only || remote_only;
@@ -15,50 +67,13 @@ pub fn ls_remotes(path: String, repository_only: bool, domain: String, user: Str
     paths.sort_by(|a, b| a.to_str().cmp(&b.to_str()));
 
     for path in paths {
-        let dir_path = path.clone();
-        let dir_path = dir_path.to_str().unwrap();
-        let mut dir_path = home_dir_mark(dir_path).expect("");
-
-        let repo = match Repository::open(path) {
-            Ok(data) => data,
-            Err(_) => {
-                if repository_only {
-                    continue;
-                }
-                print_ls_item(dir_path.as_str(), false, None, None);
-                continue;
-            }
-        };
-
-        let remote_names = repo.remotes().unwrap();
-        let remote_names: Vec<&str> = remote_names.iter().map(|s| s.unwrap()).collect();
-        let mut remotes: HashMap<String, String> = HashMap::new();
-        for name in remote_names {
-            let url = repo.find_remote(name).unwrap().url().unwrap().to_string();
-            remotes.insert(name.to_string(), url);
-        }
-
-        for (name, url) in &remotes {
-            let url = get_git_url(url);
-            match url {
-                Some(url) => {
-                    if !domain.is_empty() && !domain.eq(&url.domain) {
-                        continue;
-                    }
-
-                    if !user.is_empty() && !user.eq(&url.user) {
-                        continue;
-                    }
-
-                    print_ls_item(dir_path.as_str(), true, Some(name.to_string()), Some(url));
-                }
-                None => {
-                    print_ls_item(dir_path.as_str(), false, None, None);
-                }
-            }
-
-            dir_path = String::new();
-        }
+        proc_dir(
+            path,
+            repository_only,
+            remote_only,
+            domain.clone(),
+            user.clone(),
+        );
     }
 
     Ok(())
